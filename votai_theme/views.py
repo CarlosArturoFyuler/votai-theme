a7d7f770b05435b4311d40be0f460c1d1b6f39a6
Felipe Álvarez <luisfelipealvarez@gmail.com>
2015-06-30 22:07:53 -0300

    Arreglando para que el JSON que se devuelve sea de la forma como esta https://gist.github.com/lfalvarez/78992ce8eff1bab5f59f

--- votai_theme/views.py
+++ votai_theme/views.py
@@ -13,44 +13,57 @@ class MedianaranjaJsonView(MediaNaranjaView):
         super(MedianaranjaJsonView, self).dispatch(*args, **kwargs)
         election = self.object
         response = {
-            'election_id': election.id,
-            "election_name": election.name,
-            "district": election.area.name,
-            "level": election.extra_info['nivel'],
-            "post": election.extra_info['cargo'],
-            "categories": [],
-            "candidates": []
+            'Id': election.id,
+            "Nombre": election.name,
+            "Distrito": election.area.name,
+            "Nivel": election.extra_info['nivel'],
+            "Cargo": election.extra_info['cargo'],
+            "Preguntas": {
+                "Id": 0,
+                "Nro_Preguntas": 0,
+                "Nro_Candidatos": 0,
+                "Categorias": [],
+                "Candidatos": []
+            }
 
         }
         for category in election.categories.all():
-            category_dict = {'category_id': category.id,
-                             'name': category.name,
-                             'questions': []
+            category_dict = {'category_id': category.slug,
+                             'Texto': [],
+                             'Opciones': []
                              }
             for topic in category.topics.all():
-                question_dict = {"question_id": topic.id,
-                                 "question_text": topic.label,
-                                 "answers": []
+                question_dict = {"Id": topic.slug,
+                                 "Texto": topic.label,
+                                 "Opciones": []
                                  }
+                response['Preguntas']["Nro_Preguntas"] += 1
+                topic_positions = []
                 for position in topic.positions.all():
                     position_dict = {"answer_id": position.id,
                                      "answer_text": position.label,
                                      "answer_value": position.answervalue.value
                                      }
-                    question_dict['answers'].append(position_dict)
-                category_dict['questions'].append(question_dict)
-            response['categories'].append(category_dict)
+                    topic_positions.append({
+                        "Id": position.id,
+                        "Texto": position.label})
+                category_dict['Opciones'].append(topic_positions)
+                # response['Preguntas']['Categorias'].append(question_dict)
+                category_dict['Texto'].append(topic.label)
+            response['Preguntas']['Categorias'].append(category_dict)
+        response['Preguntas']["Nro_Candidatos"] = election.candidates.count()
         for candidate in election.candidates.all():
-            candidate_dict = {'candidate_id': candidate.id,
-                              'candidate_name': candidate.name,
-                              'candidate_pic': candidate.image,
-                              'positions': []
+            candidate_dict = {'Id': candidate.id,
+                              'Nombre': candidate.name,
+                              'Foto': candidate.image,
                               }
             taken_positions = TakenPosition.objects.filter(position__topic__category__in=election.categories.all(), person=candidate)
             for taken_position in taken_positions:
                 taken_position_dict = {'question_id': taken_position.topic.id,
                                        'answer_id': taken_position.position.id
                                        }
-                candidate_dict['positions'].append(taken_position_dict)
-            response['candidates'].append(candidate_dict)
+                if taken_position.topic.category.slug not in candidate_dict.keys():
+                    candidate_dict[taken_position.topic.category.slug] = []
+                candidate_dict[taken_position.topic.category.slug].append(taken_position.position.id)
+            response['Preguntas']['Candidatos'].append(candidate_dict)
         return JsonResponse(response)
